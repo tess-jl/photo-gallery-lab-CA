536284a2cd930b86634ab18db5b974bb89d58bac
Tess Lameyer <tjlameyer@gmail.com>
2019-10-30 10:00:12 -0700

    updated header

--- home/Header.js
+++ home/Header.js
@@ -3,10 +3,10 @@ import Component from '../Component.js';
 class Header extends Component {
     renderHTML() {
         return /*html*/`
-            <header>
-                <img src="../data/logo.jpg" alt="logo">
-                <h1>Photo Gallery</h1>
-            </header>
+        <nav>
+            <img id="logo" src="./data/logo.jpg">
+            <h1>Photo Gallery</h1>
+        </nav>
         `;
     }
 }
